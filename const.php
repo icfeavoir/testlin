936d1188c99d3b026bc8e4b470424e2e0f1d0851
Unknown <pierrotleroy@hotmail.fr>
2017-09-13 16:04:31 +0200

    whatever

--- const.php
+++ const.php
@@ -1,17 +1,19 @@
 <?php
 	$accounts = array(
-			'user'=>'pass';
+			'hirehore@zipcad.com'		=> 'testlinkedin01',	//Last Account
+
+			'karina@youpic.com'			=> 'wilfagamechange2015	',		// REAL ACCOUNT
 		);
 
 	$mails = array_keys($accounts);
 	$pass = array_values($accounts);
 
-	$selected_account = 0;
+	$selected_account = 1;
 
 	define('USERNAME', $mails[$selected_account]);
 	define('PASSWORD', $pass[$selected_account]);
 
-	define('HOST', '');
-	define('DB_NAME', '');
-	define('DB_USER', '');
-	define('DB_PASSWORD', '');
+	define('HOST', 'localhost');
+	define('DB_NAME', 'linkedinBot');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'root');
