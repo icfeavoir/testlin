93ab625cead8d6b7a54edee5ec77836cd7b3f767
Unknown <pierrotleroy@hotmail.fr>
2017-09-27 13:32:05 +0200

    Default files

---
+++ const.php
@@ -0,0 +1,14 @@
+<?php
+	define('ROOTPATH', __DIR__);
+
+	define('USERNAME', '');
+	define('PASSWORD', '');
+
+	define('WATSON_USERNAME', '');
+	define('WATSON_PASSWORD', '');
+	define('WATSON_CONVERSATION', '');
+
+	define('HOST', '');
+	define('DB_NAME', '');
+	define('DB_USER', '');
+	define('DB_PASSWORD', '');
