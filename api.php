26c793b9018d93bbe88f6a015d75f8f6d9d8ef6c
Your Name <you@example.com>
2017-11-23 13:57:34 +0000

    fix bug api

--- api.php
+++ api.php
@@ -2,10 +2,10 @@
     require_once('const.php');
     require_once('db.php');
 
-    if(isset($_POST['email'] && isset($_POST['password']))){
+    if(isset($_POST['email']) && isset($_POST['password'])){
     	$request = $db->prepare('SELECT ID FROM accounts WHERE email=:email AND password=:password LIMIT 1');
     	$request->execute(array(':email'=>$_POST['email'], ':password'=>$_POST['password']));
-    	if($request->rowCount == 0){
+    	if($request->rowCount() == 0){
     		echo 'error';
     	}else{
     		echo $request->fetch()['ID'];
