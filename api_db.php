b470cda22d000e7a04f738e7b310da001e675ecc
Pierre Leroy <pierrotleroy@hotmail.fr>
2017-12-30 18:19:14 +0100

    API should be ready

--- api_db.php
+++ api_db.php
@@ -4,22 +4,9 @@
 
     $post = json_decode(file_get_contents("php://input"));
 
-
-
     if(isset($post->api_db) && isset($post->function)){
-        // $data = "";
-        // if(isset($post->data)){
-        //     foreach ($post->data as $key => $value) {
-        //         $data .= $value.",";
-        //     }
-        // }
         $function = $post->function;
-        echo $function(...$post->data);
+        echo json_encode($function(...$post->data));
     }else{
         print_r($post);
-    }
-
-
-    function test($p1, $p2){
-        return $p1+$p2;
     }
